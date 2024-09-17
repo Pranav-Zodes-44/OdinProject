41f33025b24d13bfe98e4c529c36a9dfc707ecaa
Pranav Reddy <pranav@quicket.co.za>
2024-09-17 19:20:27 +0200

    linting: converting indentation to tabs

--- objects_object_constructors/Book.js
+++ objects_object_constructors/Book.js
@@ -1,14 +1,14 @@
 export default function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
 
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read'}`
-    }
+	this.info = function() {
+		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read'}`
+	}
 
-    this.toggleRead = function() {
-        this.read = !this.read;
-    }
+	this.toggleRead = function() {
+		this.read = !this.read;
+	}
 }
